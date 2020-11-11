7d0e89dd252e0dc534456da5887fb7933c4615bd
julen5 <72442234+julen5@users.noreply.github.com>
2020-11-11 11:11:17 +0100

    Add files via upload

---
+++ Tarjeta.php
@@ -0,0 +1,87 @@
+<html>
+<?php
+$res = json_decode( file_get_contents('http://localhost:4000/infocards/user1/'), true );
+$encrypted = $res;
+$clave = 'KeyMustBe16ByteOR24ByteOR32Byte!';
+$decrypted = @openssl_decrypt($encrypted, 'aes-256-ecb', $clave);
+$res = json_decode($decrypted, true);
+$lastactive_eur = $res['lastactive_eur'];
+$lastactive_int = $res['lastactive_int'];
+$card_europe = $res['card_europe'];
+$card_international = $res['card_international'];
+$current_card = $res['current_card'];
+?>
+    <form method="post" name="tarjetas" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    Cambiar tarjeta:<input type="submit" name="cambiar" value="Activar"/>
+    </form>
+<p>Tarjeta actual <?php echo $current_card; ?></p>
+    <table border="1">
+    <tr>
+    <th>Ultima vez activado Europa</th>
+    <td><?php echo $lastactive_eur;?></td> 
+    </tr>
+    <tr>
+    <th>Tarjeta Europea</th>
+    <td><?php echo $card_europe;?></td> 
+    </tr>
+    <tr>
+    <th>Tarjeta actual</th>
+    <td><?php echo $current_card;?></td> 
+    </tr>
+    </table>
+<?php
+if(isset($_POST['cambiar'])) {
+    if($current_card == 'INTERNATIONAL') {
+    $res = json_decode( file_get_contents('http://localhost:4000/enablecard/user1/EUROPE'), true );
+    $res = json_decode( file_get_contents('http://localhost:4000/infocards/user1/'), true );
+   // $encrypted = $res;
+   // $clave = 'KeyMustBe16ByteOR24ByteOR32Byte!';
+   // $decrypted = @openssl_decrypt($encrypted, 'aes-256-ecb', $clave);
+   // $res = json_decode($decrypted, true); 
+?>
+    <table border="1">
+    <tr>
+    <th>Ultima vez activado Europa</th>
+    <td><?php echo $lastactive_eur;?></td> 
+    </tr>
+    <tr>
+    <th>Tarjeta Europea</th>
+    <td><?php echo $card_europe;?></td> 
+    </tr>
+    <tr>
+    <th>Tarjeta actual</th>
+    <td><?php echo $current_card;?></td> 
+    </tr>
+    </table>
+<?php
+    }
+    if(isset($_POST['cambiar'])) {    
+    if($current_card == 'EUROPE') {
+        $res = json_decode ( file_get_contents ('http://localhost:4000/enablecard/user1/INTERNATIONAL'), true);
+        $res = json_decode( file_get_contents('http://localhost:4000/infocards/user1/'), true ); 
+       // $encrypted = $res;
+       // $clave = 'KeyMustBe16ByteOR24ByteOR32Byte!';
+       // $decrypted = @openssl_decrypt($encrypted, 'aes-256-ecb', $clave);
+      //  $res = json_decode($decrypted, true);
+
+?>
+<table border="1">
+<tr>
+<th>Ultima vez activado Internacional</th>
+<td><?php echo $lastactive_int;?></td> 
+</tr>
+<tr>
+<th>Tarjeta International</th>
+<td><?php echo $card_international;?></td> 
+</tr>
+<tr>
+<th>Tarjeta actual</th>
+<td><?php echo $current_card;?></td> 
+</tr>
+</table>
+<?php
+}
+    }
+}
+?>	
+</html>
