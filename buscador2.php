61d09830fe85086ad5690e5df37ffafdbdd0d499
jonvb <72795904+jonvb@users.noreply.github.com>
2020-11-15 11:38:38 +0100

    BUscador horas vfinal

--- buscador2.php
+++ buscador2.php
@@ -3,26 +3,31 @@
 <title>Buqueda</title>
 </head>
 <body>
+<?php
+$user_id = get_current_user_id();
+?>
 <form name="form_buscar" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-  <h1>Búsqueda por Fecha</h1>
-    Fecha comienzo: <br/>
-    <input type="date" id="start_date" name="start_date"  placeholder="yyyy/mm/dd"> <br/>
-    Fecha final:<br/>
-    <input type="date" id="end_date" name="end_date"  placeholder="yyyy/mm/dd"><br/>
     <input type="hidden" id="form_sent" name="form_sent" value="true">
-    <h1>Gastos</h1>
-     Importe 1: <br/>
-    <input type="int" id="importe1" name="importe1"> <br/>
-     Importe2:<br/>
-    <input type="int" id="importe2" name="importe2"><br/>
-    <input type="hidden" id="form_sent" name="form_sent" value="true">
+  <h1>Búsqueda de horas en cada proyecto</h1></br>
+    <label>Fecha comienzo: <br/>
+    <input type="date" id="start_date" name="start_date"  placeholder="yyyy/mm/dd"> </label><br/>
+    <label>Fecha final:<br/>
+    <input type="date" id="end_date" name="end_date"  placeholder="yyyy/mm/dd"></label></br><br/>
+   <label> <input type="text" placeholder="Nombre del proyecto" name="nombre_proyecto"> </label><br/><br/>
+
+</p>
     <input type="submit" name="buscador" value="Buscar" />
+<br/><br/>
 </form>
 <?php
-    if(!isset($_POST['buscador'])) {
-    $con = mysqli_connect("localhost","grupo3","grupo3","elefont") 
+	if(!isset($_POST['buscador'])){
+    $con = mysqli_connect("localhost","grupo3","grupo3","wordpress") 
     or die("No se ha podido conectar a la BD");
-    $sql = "select * from dietas";
+	if ($user_id == 1){
+            $sql = "select * from gestion ";
+        }else{
+            $sql = "select * from gestion where ID like '$user_id'";
+        };
     $result = mysqli_query($con, $sql);
     if (!$result) {
         $error = mysqli_error($con);
@@ -30,106 +35,160 @@
     } else {
         ?>
 <table border="1">
-    <tr>
-    	<th>Pais</th>
-    	<th>Ciudad</th>
-    	<th>Fecha_inicio</th>
-    	<th>Fecha_fin</th>
-        <th>Continente</th>
-    	<th>Gastos</th>
-    	<th>Medio de transporte</th>
-    	<th>Peaje</th>
-        <th>Parking</th>
-    	<th>Ticket</th>
-    	<th>Otros conceptos</th>
+    <tr><th>Nombre del proyecto</th>
+    	<th>Horas</th>
+    	<th>Codigo de departamento</th>
+		<th>Codigo de proyecto</th>
+		<th>Fecha</th>
     </tr>
 <?php
 while ($row = mysqli_fetch_assoc($result)) { //mientras haya datos...
     extract($row);
 ?>  
     <tr>
-    	<td><?php echo $pais; ?></td>
-    	<td><?php echo $ciudad; ?></td> 
-    	<td><?php echo $fecha_inicio; ?></td>
-    	<td><?php echo $fecha_fin; ?></td>
-        <td><?php echo $continente; ?></td>
-    	<td><?php echo $gastos; ?></td>
-        <td><?php echo $medio_transporte; ?></td>
-    	<td><?php echo $peaje; ?></td>
-        <td><?php echo $parking; ?></td>
-        <td><?php echo $ticket; ?></td>
-    	<td><?php echo $otros_conceptos; ?></td>
-    </tr>
+	<td><?php echo $nombre_proyecto; ?></td> 
+    	<td><?php echo $horas; ?></td>
+    	<td><?php echo $codigo_departamento; ?></td> 
+		<td><?php echo $codigo_proyecto; ?></td> 
+		<td><?php echo $fecha; ?></td> 
+		</tr>
     <?php
     }
     ?>
 </table>
     <?php 
-    }
-    }
-?>
-<?php
-    if(isset($_POST['buscador'])) {
+    }}if(isset($_POST['buscador'])) {
+
+		
         $fecha_inicio=$_POST['start_date'];
         $fecha_fin=$_POST['end_date'];
-        $importe1=$_POST['importe1'];
-        $importe2=$_POST['importe2'];
-    if((!$fecha_inicio == "") && (!$fecha_fin == "")) {
-        $con = mysqli_connect("localhost","grupo3","grupo3","elefont") 
-        or die("No se ha podido conectar a la BD");
-        $sql1 = "select * from dietas where fecha_inicio and fecha_fin between '$fecha_inicio' and '$fecha_fin'";
-        $result1 = mysqli_query($con, $sql1);
-    if (!$result1) {
-        $error = mysqli_error($con);
-            echo "ERROR: $error"; 
-    } elseif((!$importe1 == "") && (!$importe2 == "")) {
-        $con = mysqli_connect("localhost","grupo3","grupo3","elefont") 
-        or die("No se ha podido conectar a la BD");
-        $sql2 = "select * from dietas where gastos between '.$importe1.' and '.$importe2.'";
-        $result2 = mysqli_query($con, $sql2);
-        if (!$result2) {
-            $error = mysqli_error($con);
-                echo "ERROR: $error"; {
+        $nombre_proyecto=$_POST['nombre_proyecto'];
+	
+		//SI SE QUIERE BUSCAR MEDIANTE IMPORTE Y FECHA A LA VEZ
+		
+				if(!empty($nombre_proyecto)&&(!empty($fecha_fin))&&(!empty($fecha_inicio))){
+				$con = mysqli_connect("localhost","grupo3","grupo3","wordpress") 
+				or die("No se ha podido conectar a la BD");
+				if ($user_id == 1){
+            $sql = "select * from gestion where nombre_proyecto like '$nombre_proyecto' AND fecha between '$fecha_inicio' and '$fecha_fin' order by fecha desc";
+        }else{
+            $sql = "select * from gestion where nombre_proyecto like '$nombre_proyecto' AND fecha between '$fecha_inicio' and '$fecha_fin' and ID like '$user_id' order by fecha desc";
+        };
+				$result1 = mysqli_query($con, $sql);
+			if (!$result1) {
+				$error = mysqli_error($con);
+				echo "ERROR: $error";}
+
+
     ?>
-    <table border="1">
-    <tr>
-    	<th>Pais</th>
-    	<th>Ciudad</th>
-    	<th>Fecha_inicio</th>
-    	<th>Fecha_fin</th>
-        <th>Continente</th>
-    	<th>Gastos</th>
-    	<th>Medio de transporte</th>
-    	<th>Peaje</th>
-        <th>Parking</th>
-    	<th>Ticket</th>
-    	<th>Otros conceptos</th>
+<table border="1">
+    <tr><th>Nombre del proyecto</th>
+    	<th>Horas</th>
+    	<th>Codigo de departamento</th>
+		<th>Codigo de proyecto</th>
+		<th>Fecha</th>
     </tr>
 <?php
 while ($row = mysqli_fetch_assoc($result1)) { //mientras haya datos...
     extract($row);
 ?>  
     <tr>
-    	<td><?php echo $pais; ?></td>
-    	<td><?php echo $ciudad; ?></td> 
-    	<td><?php echo $fecha_inicio; ?></td>
-    	<td><?php echo $fecha_fin; ?></td>
-        <td><?php echo $continente; ?></td>
-    	<td><?php echo $gastos; ?></td>
-        <td><?php echo $medio_transporte; ?></td>
-    	<td><?php echo $peaje; ?></td>
-        <td><?php echo $parking; ?></td>
-        <td><?php echo $ticket; ?></td>
-    	<td><?php echo $otros_conceptos; ?></td>
-    </tr>
-
-<?php 
-    } ?>
+	<td><?php echo $nombre_proyecto; ?></td> 
+    	<td><?php echo $horas; ?></td>
+    	<td><?php echo $codigo_departamento; ?></td> 
+		<td><?php echo $codigo_proyecto; ?></td> 
+		<td><?php echo $fecha; ?></td> 
+		</tr>
+    <?php
+    }
+    ?>
 </table>
 <?php
+		//IMPORTE FORM
+		
+	}else if(!empty($nombre_proyecto)){
+				$con = mysqli_connect("localhost","grupo3","grupo3","wordpress") 
+				or die("No se ha podido conectar a la BD");
+				if ($user_id == 1){
+            $sql = "select * from gestion where nombre_proyecto like '$nombre_proyecto' order by fecha desc";
+        }else{
+            $sql = "select * from gestion where nombre_proyecto like '$nombre_proyecto' order by fecha desc";
+        };
+				$result1 = mysqli_query($con, $sql);
+			if (!$result1) {
+				$error = mysqli_error($con);
+				echo "ERROR: $error";}
+
+
+   
+    ?>
+<table border="1">
+    <tr><th>Nombre del proyecto</th>
+    	<th>Horas</th>
+    	<th>Codigo de departamento</th>
+		<th>Codigo de proyecto</th>
+		<th>Fecha</th>
+    </tr>
+<?php
+while ($row = mysqli_fetch_assoc($result1)) { //mientras haya datos...
+    extract($row);
+?>  
+    <tr>
+	<td><?php echo $nombre_proyecto; ?></td> 
+    	<td><?php echo $horas; ?></td>
+    	<td><?php echo $codigo_departamento; ?></td> 
+		<td><?php echo $codigo_proyecto; ?></td> 
+		<td><?php echo $fecha; ?></td> 
+		</tr>
+    <?php
     }
+    ?>
+</table>
+<?php
+
+// FECHA FORM
+
+    }else if(!empty($fecha_inicio)&&(!empty($fecha_fin))){
+				$con = mysqli_connect("localhost","grupo3","grupo3","wordpress") 
+				or die("No se ha podido conectar a la BD");
+				if ($user_id == 1){
+            $sql = "select * from gestion where fecha between '$fecha_inicio' and '$fecha_fin' order by fecha desc ";
+        }else{
+            $sql = "select * from gestion where fecha between '$fecha_inicio' and '$fecha_fin' and ID like '$user_id' order by fecha desc ";
+        };
+				$result1 = mysqli_query($con, $sql);
+			if (!$result1) {
+				$error = mysqli_error($con);
+				echo "ERROR: $error";}
+
+
+   
+    ?>
+<table border="1">
+    <tr><th>Nombre del proyecto</th>
+    	<th>Horas</th>
+    	<th>Codigo de departamento</th>
+		<th>Codigo de proyecto</th>
+		<th>Fecha</th>
+    </tr>
+<?php
+while ($row = mysqli_fetch_assoc($result1)) { //mientras haya datos...
+    extract($row);
+?>  
+    <tr>
+	<td><?php echo $nombre_proyecto; ?></td> 
+    	<td><?php echo $horas; ?></td>
+    	<td><?php echo $codigo_departamento; ?></td> 
+		<td><?php echo $codigo_proyecto; ?></td> 
+		<td><?php echo $fecha; ?></td> 
+		</tr>
+    <?php
     }
-}   
+    ?>
+</table>
+	<?php }}
+    
+	
 ?>                                                                      
 </body>
 </html>
